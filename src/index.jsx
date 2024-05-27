63c7f0805df91f0ca75c68333ed1189a191b39bf
DutchArtGames <arthurhoogerbrug@live.nl>
2024-05-27 20:33:54 +0200

    modified:   src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorkerRegistration from '../public/service-worker';
+import * as serviceWorker from '/service-worker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	</React.StrictMode>
 );
 
-serviceWorkerRegistration.register();
+serviceWorker.register();
