a29dbf68847bedfc5307556da632a2df34cebc09
DutchArtGames <arthurhoogerbrug@live.nl>
2024-05-27 22:35:36 +0200

    modified:   src/serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -31,7 +31,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `${publicUrl}/service-worker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
