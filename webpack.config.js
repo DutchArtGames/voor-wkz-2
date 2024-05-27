0943f5142a580c2524766a82ed52f986ac95751e
DutchArtGames <arthurhoogerbrug@live.nl>
2024-05-27 22:52:04 +0200

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-  entry: './src/service-worker.js',
+  entry: './public/service-worker.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'service-worker.js'
